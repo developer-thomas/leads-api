3cb2baa80b3d68318a422da530eb218f87d37e22
Thomas Edson <andepipous@gmail.com>
2024-12-21 21:41:48 -0400

    fix: fix errors handler on leads-controller

--- src/controllers/leads-controller.ts
+++ src/controllers/leads-controller.ts
@@ -1,9 +1,10 @@
 import { Handler } from "express";
 import { prisma } from "../database";
-import { CreateLeadRequestSchema } from "./schemas/LeadsRequestSchema";
+import { CreateLeadRequestSchema, UpdateLeadRequestSchema } from "./schemas/LeadsRequestSchema";
 import { HttpError } from "../errors/HttpError";
 
 export class LeadsController {
+  // Retorna todos os leads
   public index: Handler = async (req, res, next) => {
     try {
       const leads = await prisma.lead.findMany();
@@ -13,18 +14,7 @@ export class LeadsController {
     }
   };
 
-  public create: Handler = async (req, res, next) => {
-    try {
-      const body = CreateLeadRequestSchema.parse(req.body);
-      const newLead = await prisma.lead.create({
-        data: body,
-      });
-      res.status(201).json(newLead);
-    } catch (error) {
-      next(error);
-    }
-  };
-
+  // Pega um lead através do id
   public show: Handler = async (req, res, next) => {
     try {
       const lead = await prisma.lead.findUnique({
@@ -38,6 +28,64 @@ export class LeadsController {
       if (!lead) throw new HttpError(404, "Lead não encontrado!");
 
       res.json(lead);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  // Cria um lead
+  public create: Handler = async (req, res, next) => {
+    try {
+      const body = CreateLeadRequestSchema.parse(req.body);
+      const newLead = await prisma.lead.create({
+        data: body,
+      });
+      res.status(201).json(newLead);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  // Atualiza um lead
+  public update: Handler = async (req, res, next) => {
+    try {
+      const id = parseInt(req.params.id);
+      if (!id) throw new HttpError(404, "Lead inválido");
+
+      const body = UpdateLeadRequestSchema.parse(req.body);
+      if (!req.body) throw new HttpError(401, "Não pode estar vazio");
+
+      const leadExists = await prisma.lead.findUnique({ where: { id } });
+      if (!leadExists) throw new HttpError(404, "Lead não encontrado!");
+
+      const updatedUser = await prisma.lead.update({
+        data: body,
+        where: {
+          id: id,
+        },
+      });
+      res.status(200).json(updatedUser);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  // Deleta um lead
+  public delete: Handler = async (req, res, next) => {
+    try {
+      const id = parseInt(req.params.id);
+      if (!id) throw new HttpError(404, "Lead inválido");
+
+      const leadExists = await prisma.lead.findUnique({ where: { id } });
+      if (!leadExists) throw new HttpError(404, "Lead não encontrado");
+
+      const deletedUser = await prisma.lead.delete({
+        where: {
+          id: +id,
+        },
+      });
+
+      res.status(200).json({ message: "User deleted", user: deletedUser });
     } catch (error) {
       next(error);
     }
--- src/controllers/schemas/LeadsRequestSchema.ts
+++ src/controllers/schemas/LeadsRequestSchema.ts
@@ -6,3 +6,10 @@ export const CreateLeadRequestSchema = z.object({
   phone: z.string(),
   status: z.enum(["New", "Contacted", "Qualified", "Converted", "Unresposive", "Desqualified", "Archived"]).optional(),
 });
+
+export const UpdateLeadRequestSchema = z.object({
+  name: z.string().optional(),
+  email: z.string().optional(),
+  phone: z.string().optional(),
+  status: z.enum(["New", "Contacted", "Qualified", "Converted", "Unresposive", "Desqualified", "Archived"]).optional(),
+});
