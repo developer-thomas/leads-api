a9df55dc77838f62d1f57b2ca795a3638c8626b6
Thomas Edson <andepipous@gmail.com>
2024-12-23 08:02:26 -0400

    feat: create groups router and its controller

--- src/router.ts
+++ src/router.ts
@@ -1,16 +1,26 @@
 import { Router } from "express";
 import { HttpError } from "./errors/HttpError";
 import { LeadsController } from "./controllers/leads-controller";
-
+import { GroupsController } from "./controllers/groups-controller";
 const router = Router();
 
 const leadsController = new LeadsController();
+const groupsController = new GroupsController();
 
+// Rotas de Leads
 router.get("/leads", leadsController.index);
 router.post("/leads", leadsController.create);
 router.get("/leads/:id", leadsController.show);
 router.put("/leads/:id", leadsController.update);
 router.delete("/leads/:id", leadsController.delete);
+
+// Rotas de Grupos
+router.get("/groups", groupsController.index);
+router.post("/groups", groupsController.create);
+router.get("/groups/:id", groupsController.show);
+router.put("/groups/:id", groupsController.update);
+router.delete("/groups/:id", groupsController.delete);
+
 router.get("/status", async (req, res, next) => {
   try {
     res.json({ message: "OK" });
